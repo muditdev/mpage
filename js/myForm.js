ed77cf917ce08c49478bb63f27247a7fd1b1be4d
muditjaingit <1996mudit1502@gmail.com>
2015-06-17 10:07:32 +0530

    updating

---
+++ js/myForm.js
@@ -0,0 +1,30 @@
+
+	var name_1=document.getElementById('full_name');
+	var email_1=document.getElementById('e_mail');
+	var massage_1=document.getElementById('massage');
+function for_name(){
+	if(name_1.value=='Full Name'){
+		name_1.value='';
+		}
+	else{
+		name_1.value=name_1.value;
+	}
+}
+
+function for_email(){
+	if(email_1.value=='Email Address'){
+		email_1.value='';
+		}
+	else{
+		email_1.value=email_1.value;
+	}
+}
+
+function for_massage(){
+	if(massage_1.value=='Massage'){
+		massage_1.value='';
+		}
+	else{
+		massage_1.value=massage_1.value;
+	}
+}
